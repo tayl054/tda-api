b49b59cd5f97887c0247fc71a9087353c0f7a98d
Greg Taylor <gregtaylor999@gmail.com>
2023-02-20 22:26:02 +0000

    test2

--- examples/testlogin.py
+++ examples/testlogin.py
@@ -1,15 +1,15 @@
-from tda import auth, client
 import json
+from tda import auth, client
 
-token_path = 'token'
-#token_path = 'C:\Users\GREG\VS_CODE\tda-api\tda-api'
+token_path = '/path/to/token.json'
 api_key = 'QCGZOCCCTTOAXUV1XNMNJW5FJLINKCM8@AMER.OAUTHAP'
 redirect_uri = 'https://localhost'
 try:
     c = auth.client_from_token_file(token_path, api_key)
 except FileNotFoundError:
+    
     from selenium import webdriver
-    with webdriver.Chrome(executible_path="C:\\Users\\tayl054\\VS_Projects\\tda-api\\chromedriver") as driver:
+    with webdriver.Chrome() as driver:
         c = auth.client_from_login_flow(
             driver, api_key, redirect_uri, token_path)
 
@@ -18,8 +18,5 @@ r = c.get_price_history('AAPL',
         period=client.Client.PriceHistory.Period.TWENTY_YEARS,
         frequency_type=client.Client.PriceHistory.FrequencyType.DAILY,
         frequency=client.Client.PriceHistory.Frequency.DAILY)
-#assert r.ok, r.raise_for_status()
-
-#df = r.json()
-#print(df)
+assert r.status_code == 200, r.raise_for_status()
 print(json.dumps(r.json(), indent=4))
